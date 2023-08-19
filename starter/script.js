d4b187bb47451c59fd05a09537aa9096d19b918d
Jobear <jamescodger18@gmail.com>
2023-08-19 21:07:37 +0600

    Prac code

---
+++ starter/script.js
@@ -0,0 +1,124 @@
+
+
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+console.log("James");
+console.log(23);
+
+let firstName = "Jobear";
+console.log(firstName);
+
+let myFirstJob = "Programmer";
+let myCurrentJob = 'Codder';
+
+console.log(myFirstJob);
+console.log(myCurrentJob);
+
+
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'james');
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(year);
+console.log(typeof year);
+
+console.log(typeof null);
+
+
+
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990;
+console.log(birthYear);
+console.log(typeof birthYear);
+
+// const job;
+
+var job = 'programmer';
+job = 'teacher';
+
+let lastName = 'Al Mahomud';
+console.log(lastName);
+
+
+
+//lacture 09
+// math operator
+const now = 2030;
+const ageJames = now - 1999;
+const ageMonika = now - 2005;
+
+console.log(ageJames, ageMonika);
+
+console.log(ageJames * 2, ageMonika / 3, 2 ** 4);
+//2**4 mens  2 to the power of 4 = 2*2*2*2
+
+const firstName = 'Jobear';
+const lastName = 'James';
+console.log(firstName + lastName);
+
+// Assaignment operator
+let x = 10 + 5;
+x += 10;
+x *= 4;
+x /= 2;
+x++;
+x--;
+console.log(x);
+
+//Comparison Operator
+console.log(ageJames > ageMonika);
+console.log(ageJames >= 32);
+
+const isFullAge = ageMonika >= 25;
+
+console.log(now - 1991 > now - 2018);
+
+
+
+//lecture 10
+
+const now = 2030;
+const ageJames = now - 1999;
+const ageMonika = now - 2005;
+
+
+// console.log(25 - 10 - 5);
+
+let x, y;
+x = y = 25 - 10 - 5;
+console.log(x, y);
+
+const avarageAge = (ageJames + ageMonika) / 2;
+console.log(ageJames, ageMonika, avarageAge);
+
+
+
+// lecture 12
+
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const BMIMark = massMark / heightMark ** 2;
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+const markHigherBMI = BMIMark > BMIJohn;
+
+console.log(BMIMark, BMIJohn, markHigherBMI);
